c6b982ac95f5ee1037fd57d6041c21569887f2b3
Ronak Doshi <ronakvipul.doshi@iiitb.org>
2018-11-29 22:15:57 +0530

    Adding tast 7

--- v1/SymmetricSquareMatrix.h
+++ v1/SymmetricSquareMatrix.h
@@ -6,12 +6,27 @@
 class SymmetricSquareMatrix {
 
     private: std::vector<std::vector<double>> _data;
+             int _numRows;
+             int _numColums;   
 
-    public: SymmetricSquareMatrix(std::vector<std::vector<double>> d);
+    public: SymmetricSquareMatrix(std::vector<std::vector<double>> d){
+                _data = d;
+                _numRows = d.size();
+                _numColums = d[0].size();
+            }
             SymmetricSquareMatrix(){}
             SymmetricSquareMatrix(const SymmetricSquareMatrix&);
             ~SymmetricSquareMatrix();
-            double getElementOfMatrix(int, int);
+            double getElementOfMatrix(int i, int j){
+                return _data[i][j];
+            }
+            int getNumRows(){return _numRows;}
+            int getNumColums() { return _numColums; }
+            void setElementOfMatrix(int i,int j,double value){
+                _data[i][j] = value;
+            }
+            void setNumRows(int rows){_numRows = rows;}
+            void setNumColums(int colums) { _numColums = colums; }
 };
 
 #endif // !SYMMETRIC_SQUARE_MATRIX_H_
