52c5a374a0c3a2dfb7eb1c5ed47f8064d6ed4d03
advait-l <advait.lonkar@iiitb.org>
2018-12-05 22:25:46 +0530

    updated task4

--- v1/implementation/Task4/Variable.h
+++ v1/implementation/Task4/Variable.h
@@ -12,6 +12,7 @@ class Variable
     private: int _varIndex;
              int _numOfValues;
              double _variance;
+             double _mean;
              std::vector<double> _values;
 
              
@@ -19,25 +20,25 @@ class Variable
             void setVariable(int, Matrix);
             int get_varIndex() { return _varIndex; }
             void set_varIndex(int in) { _varIndex = in;}
-            double get_variance() { return _variance; }
+            double get_variance() const { return _variance; }
             void set_variance(double var) { _variance = var; }
+            double get_mean() const { return _mean; }
+            void set_mean(double x) { _mean = x; }
             int get_numOfValues() { return _numOfValues; }
             int get_size() { return _values.size(); }
             void set_values(std::vector<double> values) { _values = values; }
+            void computeVariance(std::vector<Variable>);
             std::vector<double> get_values() { return _values; }
+
+
+
 
     friend std::ostream& operator<<(std::ostream&, const Variable&);
 
 
 };
 
-class varianceSort
-{
-    inline bool operator() (const Variable& v1, const Variable& v2)
-    {
-        return (v1.get_variance() < v2.get_variance());
-    }
-};
+bool operator<(const Variable &v1, const Variable &v2);
 
 #endif //VARIABLE_H_
 
